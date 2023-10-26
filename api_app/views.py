bff8639212ca3719a252bbdb62df8e33b7be1304
HitzCommitted <hitzcommitted@gmail.com>
2023-10-26 16:30:57 +0300

    working delete

--- api_app/views.py
+++ api_app/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import CartItemSerializer
 from .models import CartItem
+from django.shortcuts import get_object_or_404
 
 
 # Create your views here.
@@ -45,3 +46,8 @@ class CartItemViews(APIView):
             return Response({"status": "success", "data": serializer.data})
         else:
             return Response({"status": "error", "data": serializer.errors})
+
+    def delete(self, request, id=None):
+        item = get_object_or_404(CartItem, id=id)
+        item.delete()
+        return Response({"status": "success", "data": "Item Deleted"})
